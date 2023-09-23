02ecda3b40d3a16ee4abe5e2e4fb9f68bc8c1936
Sami <sxr089@student.bham.ac.uk>
2023-09-23 10:40:43 +0200

    new database set up

--- react_backend/settings_local.py
+++ react_backend/settings_local.py
@@ -1,5 +1,3 @@
-from decouple import Config as DecoupleConfig
-config = DecoupleConfig()
 from .settings import *
 
 DEBUG = True
@@ -7,10 +5,10 @@ DEBUG = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
+        'NAME': 'd7et0ekb5ek6o5',
+        'USER': 'wlatjsrmbdtakt',
+        'PASSWORD': '13299bac0074d0ab5a99deef2bc90b2673b61e7af803134c72de8515b6776620',
+        'HOST': 'ec2-54-195-144-105.eu-west-1.compute.amazonaws.com',
+        'PORT': '5432',
     }
 }
