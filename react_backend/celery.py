28d91d679c1209220c6db028c6d687abdcd15b43
Sami <sxr089@student.bham.ac.uk>
2023-09-23 16:16:47 +0200

    celery bug

--- react_backend/celery.py
+++ react_backend/celery.py
@@ -2,13 +2,26 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
-from react_backend import settings
+from react_backend import settings  # Ensure this is the correct import for your settings module
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'react_backend.settings')
 
 app = Celery('react_backend')
 
+# Load task modules from all registered Django app configs.
 app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Explicitly set the broker URL from Django settings
 app.conf.broker_url = settings.CELERY_BROKER_URL
+
+# Set broker_connection_retry_on_startup to True to retain retrying connections on startup behavior
+app.conf.broker_connection_retry_on_startup = True
+
+# Auto-discover tasks in all installed apps
 app.autodiscover_tasks()
+
+# Optional: Define a task that will print the message "hello" to the console
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
